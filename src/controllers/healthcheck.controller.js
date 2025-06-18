c95929a0e49b63b0eb666def08457f234d332f1d
shubhikhandelwal <shubhik2503@gmail.com>
2025-06-18 15:40:47 +0530

    Add healthcheck controller and routes for service health status

---
+++ src/controllers/healthcheck.controller.js
@@ -0,0 +1,17 @@
+import {apiError} from "../utils/apiError.js"
+import {apiResponse} from "../utils/apiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+
+const healthcheck = asyncHandler(async (req, res) => {
+    //TODO: build a healthcheck response that simply returns the OK status as json with a message
+    res.status(200)
+    .json(
+        new apiResponse(200 , {} , "service is healthy")
+    )
+})
+
+export {
+    healthcheck
+    }
+    
